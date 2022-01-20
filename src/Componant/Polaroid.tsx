9f4c97b942097de832920bed3f3f83a143cf7f34
Jimmy <jimmy.ganci@gmail.com>
2022-01-20 22:07:59 +0100

    exemple data project

--- src/Componant/Polaroid.tsx
+++ src/Componant/Polaroid.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import LiveUp1 from '../../assets/LiveUp1.jpg';
 
+// Importe tes props içi
+// const Polaroid = ({ project }: { project: object }) => {
 const Polaroid = () => {
   return (
     <div className="polaroid">
       <div className="polaroid__photo">
         <img src={LiveUp1} alt="test" />
+        {/* Distribue les informations */}
+        {/* <img src={project.image} alt={project.title} />  */}
       </div>
       <div className="polaroid__text">
-        <p>LIVE UP </p>
+        {/* distribue les informations */}
+        {/* <p>{project.title}</p> */}
+        <p>Live Up</p>
       </div>
     </div>
   );
--- src/Componant/Portfolio.tsx
+++ src/Componant/Portfolio.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 
+// import LiveUp from '../../assets/LiveUp1.jpg';
 import Polaroid from './Polaroid';
 
 const Portfolio = () => {
+  //Créer un tableau d'object exemple:
+  //Tu peut également l'importer depuis un autre fichier
+  //   const projects = [
+  //     {
+  //       idProject: 1,
+  //       title: 'projet 1',
+  //       image: LiveUp,
+  //     },
+  //     {
+  //       idProject: 2,
+  //       title: 'projet 2',
+  //       image: LiveUp,
+  //     },
+  //     {
+  //       idProject: 3,
+  //       title: 'projet 3',
+  //       image: LiveUp,
+  //     },
+  //     {
+  //       idProject: 4,
+  //       title: 'projet 4',
+  //       image: LiveUp,
+  //     },
+  //   ];
   return (
     <div className="portfolio">
       <div className="portfolio__polaroids">
@@ -10,6 +35,10 @@ const Portfolio = () => {
         <Polaroid />
         <Polaroid />
         <Polaroid />
+        {/* Fait un map de ton tableau */}
+        {/* {projects.map((project) => (
+          <Polaroid key={project.idProject} project={project} />
+        ))} */}
       </div>
       <div className="portfolio__title">
         <h2 className="portfolio__title__project">PORTFOLIO</h2>
