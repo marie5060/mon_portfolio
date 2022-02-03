d1fb6d36af4a7f13c574275fdec9e5c763884ebf
marie5060 <marie5060hotmail.com>
2022-02-03 21:24:06 +0100

    link in  portfolio added

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default {
-  // ...
+  base: `/portfolio/`,
   plugins: [
     svgrPlugin({
       svgrOptions: {
